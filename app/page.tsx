99a35584d2d5a53527fd150f073dcc2a7eb46775
ramoncodevale <ramonvalentim88@gmail.com>
2024-12-09 14:26:00 -0300

    feat: add Card Components

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ import NavBar from "./_components/navbar";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import { Check } from "lucide-react";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardTitle,
+} from "./_components/ui/card";
 
 const Home = () => {
   return (
@@ -52,6 +58,65 @@ const Home = () => {
             <p className="text-3xl text-gray-600">Cancele quando quiser</p>
             <Check className="text-customGreen" />
           </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center p-52">
+        <h1 className="text-6xl font-bold text-black">
+          Preço Simples e Transparente
+        </h1>
+        <p className="mt-4 text-center text-xl text-gray-600">
+          Pra que inúmeros planos quando nós sabemos exatamente, o que é melhor
+          para você? <br /> Assine o nosso plano mensal Pro Premium VIP e
+          garanta mensalmente um ebook novo de
+          <br /> programação. E por menos de um café por dia.
+        </p>
+
+        <div className="p-20">
+          <Card className="h-[426px] w-[418px]">
+            <CardTitle>
+              <h3 className="mt-5 text-center text-2xl font-semibold text-black">
+                Plano Pro Premium VIP
+              </h3>
+              <p className="mt-3 text-center text-sm text-customGray">
+                Tudo que você precisa para seus estudos
+              </p>
+            </CardTitle>
+
+            <CardDescription className="mt-10 flex flex-col pl-20">
+              <h3 className="text-4xl font-semibold text-black">
+                R$ 29<span className="text-xl text-gray-600">/mês</span>
+              </h3>
+
+              <div className="mt-5 flex items-center gap-2">
+                <Check className="text-customGreen" />
+                <p className="text-sm text-gray-600">1 ebook por mês</p>
+              </div>
+
+              <div className="mt-3 flex items-center gap-2">
+                <Check className="text-customGreen" />
+                <p className="text-sm text-gray-600">Curadoria Especial</p>
+              </div>
+
+              <div className="mt-3 flex items-center gap-2">
+                <Check className="text-customGreen" />
+                <p className="text-sm text-gray-600">Acesso ilimitado</p>
+              </div>
+
+              <div className="mt-3 flex items-center gap-2">
+                <Check className="text-customGreen" />
+                <p className="text-sm text-gray-600">
+                  Cancele a qualquer momento
+                </p>
+              </div>
+            </CardDescription>
+
+            <CardFooter className="mt-14 flex items-center justify-center">
+              <Button className="h-[40px] w-[332px] rounded-md bg-black">
+                Assine Agora
+              </Button>
+            </CardFooter>
+          </Card>
         </div>
       </div>
     </section>
